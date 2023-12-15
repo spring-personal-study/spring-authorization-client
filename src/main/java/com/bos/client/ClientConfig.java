6c3074089127d3b0a654ca0a8210236c0e4e8f7a
mclee <myoungcheol.lee@bluebirdcorp.com>
2023-12-15 17:11:55 +0900

    feat: implementation /v1/assets

--- src/main/java/com/bos/client/ClientConfig.java
+++ src/main/java/com/bos/client/ClientConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
