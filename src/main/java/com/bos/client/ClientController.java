2127f23c078f09f1c1837ce9beb68b2a9a44bdf8
mclee <myoungcheol.lee@bluebirdcorp.com>
2025-07-22 11:37:11 +0900

    feat: impl registering client

--- src/main/java/com/bos/client/ClientController.java
+++ src/main/java/com/bos/client/ClientController.java
@@ -2,8 +2,6 @@ package com.bos.client;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ClientController {
